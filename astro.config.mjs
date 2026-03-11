97ab92aeb6c6123a07ca9e8906258776b32610dd
Diego Martín García <dmartingarcia07@gmail.com>
2026-03-11 18:08:21 +0100

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   adapter: vercel(),
 });
