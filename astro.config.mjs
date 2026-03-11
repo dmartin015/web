2d58c94111e14f376bfb6ab8c369508cdb6f31ec
Diego Martín García <dmartingarcia07@gmail.com>
2026-03-11 21:27:46 +0100

    tweak: Se ha convertido la web a dinámica.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
